51f4bfc5891c823057963c1926088c6fe6a32c90
Yoshiaki Shinkawa <y_snkw@Y-snkw.local>
2017-11-10 00:16:33 +0900

    [add]スタート画面とスタートボタンからの画面遷移

---
+++ MaseUnityVoiceControl/Assets/Scripts/TitleController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleController : MonoBehaviour {
+
+	public void OnStartButtonClicked(){
+		Application.LoadLevel ("main_scene");
+	}
+}
